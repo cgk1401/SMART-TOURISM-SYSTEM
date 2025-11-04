bcb554d447a952a6bfc07125085517a38ab95cf9
Do Manh Cuong <doomanhcuongg@gmail.com>
2025-11-04 12:47:46 +0700

    refactor the code by seperating the login.html

--- Sightseeing/LoginScreen/urls.py
+++ Sightseeing/LoginScreen/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.function_login),
     path('check_login/', views.check_login),
-    path('register/', views.register_user)
+    #path('register/', views.register_user)
 ]
 
--- Sightseeing/LoginScreen/views.py
+++ Sightseeing/LoginScreen/views.py
@@ -1,37 +1,60 @@
-from django.http import HttpRequest
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth import authenticate
 from django.contrib.auth import login
-from django.contrib.auth.models import User
-from django.contrib import messages
-
-
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
 
 def function_login(request):
     return render(request, 'app/login.html')
+
+@csrf_exempt
 def check_login(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(username=username, password=password)
+    if request.method != "POST":
+        return JsonResponse({
+            "message": "Chỉ hỗ trợ POST",
+        }, status = 405)
+        
+    try:
+        data = json.loads(request.body.decode("utf-8"))
+        username = data.get("username", "").strip()
+        password = data.get("password", "").strip()
+        
+        user = authenticate(request, username = username, password = password)
+        
+        if user is not None:
+            login(request, user)
+            return JsonResponse({
+                "success": True,
+                "message": "Đăng nhập thành công"
+            })
 
-    if user is not None:
-        login(request, user)
-        return redirect('/MainScreen/')
-
-    else:
-        return render(request, 'login.html', {'error': 'Invalid credentials'})
-def register_user(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-
-        if User.objects.filter(username=username).exists():
-            messages.error(request, "Username already exists.")
         else:
-            User.objects.create_user(username=username, password=password)
-            messages.success(request, "Account created successfully")
-            return redirect('/')
+            return JsonResponse(
+                {
+                    "success": False,
+                    "message": "Username hoặc password sai"
+                }
+            )
+            
+    except Exception as e:
+        return JsonResponse({
+            "success": False,
+            "message": f"lỗi : {str(e)}"
+        }, status = 500)
+    
+    
+    
+# def register_user(request):
+#     if request.method == "POST":
+#         username = request.POST["username"]
+#         password = request.POST["password"]
 
-    return render(request, 'register.html')
+#         if User.objects.filter(username=username).exists():
+#             messages.error(request, "Username already exists.")
+#         else:
+#             User.objects.create_user(username=username, password=password)
+#             messages.success(request, "Account created successfully")
+#             return redirect('/')
+
+#     return render(request, 'app/register.html')
